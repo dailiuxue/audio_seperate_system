0992c68a284f334d1842b19904dab0fa4b67ce6c
dailiuxue <574525293@qq.com>
2025-03-27 14:09:56 +0800

    [Feature] Update img

--- gradio_demucs.py
+++ gradio_demucs.py
@@ -9,7 +9,7 @@ def inference(audio):
   os.system("python -m demucs.separate -n htdemucs --two-stems=vocals -d cpu test.wav -o out")
   return "./out/htdemucs/test/vocals.wav","./out/htdemucs/test/no_vocals.wav"
     
-title = "语音分类系统的设计与实现项目演示"
+title = "语音分离系统的设计与实现项目演示"
 description = "功能是基于Demucs将歌曲中的伴奏和人声分离，可以上传歌曲文件，也可以实时播放音乐.\n项目部署在笔记本上，使用的M2 CPU进行实时推理"
 
 examples=[['稻香.mp3']]
